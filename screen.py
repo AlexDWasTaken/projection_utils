1be0770e6fa0c6304819dedbdfb174bb11a32867
AlexD <xz_dy_gaming@outlook.com>
2023-07-11 01:07:49 +0800

    finish RefractionProjector

--- screen.py
+++ screen.py
@@ -71,7 +71,8 @@ class ScreenProjector():
         
 class StraightProjector():
     """
-    Assume the other person is directly on the screen.
+    Assume the other person is a smaller version of themselve except that they be proportionally reduced (denote that by k).
+    This k depends on the distance between the user and the screen (both sides)
     
     Assume full screen.
     """
@@ -109,8 +110,7 @@ class StraightProjector():
 
         return scaled_coords.T  # transpose to shape: (N, 3)
     
-    #TODO: Self.k
-    def pixel_coords_to_virtual_coords(self, real_coords, k):
+    def to_virtual_coords(self, real_coords, k):
         v_coords = real_coords * k
         return np.array([v_coords[0], -v_coords[1], -v_coords[2]])
     
@@ -126,7 +126,7 @@ class StraightProjector():
         k = (self.intrinsic[0, 0] / distance) * self.screen_width / self.width
         """
         k = (self.intrinsic[0, 0] / distance) * self.screen_width / self.width
-        virtual_remote_position = self.pixel_coords_to_virtual_coords(remote_real_coords, k)
+        virtual_remote_position = self.to_virtual_coords(remote_real_coords, k)
         gaze_vector = virtual_remote_position - current_real_coords
         gaze_vector_head = self.cam_to_head(gaze_vector)
         direction = gaze_vector_head / np.linalg.norm(gaze_vector_head)
@@ -144,8 +144,9 @@ class StraightProjector():
         
 class RefractionProjector():
     """
-    Assume the other person is behind the screen with a stretch coef k.
-    
+    Assume the other person is behind the screen with a stretch coef k. This time we look at screen as if it was a pool of water with refraction index k.
+    Still this k depends on distance(both sides)
+       
     Assume full screen.
     """
     def __init__(self, width, height, screen_width, screen_height, intrinsic: np.ndarray, reference, distance) -> None:
@@ -182,7 +183,7 @@ class RefractionProjector():
 
         return scaled_coords.T  # transpose to shape: (N, 3)
     
-    def pixel_coords_to_virtual_coords(self, real_coords, k):
+    def to_virtual_coords(self, real_coords, k):
         v_coords = real_coords * k
         return np.array([v_coords[0], -v_coords[1], -v_coords[2]])
     
@@ -197,9 +198,17 @@ class RefractionProjector():
         distance needs to be passed in.
         k = (self.intrinsic[0, 0] / distance) * self.screen_width / self.width
         """
-        k = (self.intrinsic[0, 0] / distance) * self.screen_width / self.width
-        virtual_remote_position = self.pixel_coords_to_virtual_coords(remote_real_coords, k)
-        gaze_vector = virtual_remote_position - current_real_coords
+        
+            
+        # Now that we have two points, we calculate the minimum distance between the two.
+        # We look for a path of light between the two.
+        # sin(theta_{otherside}) / sin(theta_ourside) = k
+        # However, after a bit of simple math we can find that 
+        # the intersection of the light and the screen is the intersection point 
+        # when assuming a 1:1 placement of the opposite space behind the screen 
+        # and not taking refraction into account.
+        remote_virtual_coords = self.to_virtual_coords(remote_real_coords, 1)
+        gaze_vector = remote_virtual_coords - current_real_coords
         gaze_vector_head = self.cam_to_head(gaze_vector)
         direction = gaze_vector_head / np.linalg.norm(gaze_vector_head)
         
